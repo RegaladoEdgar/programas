a46a2a04700d0d024c12b36ecdb97a7c0d43e713
Ubuntu <ubuntu@ip-172-31-55-2.us-west-2.compute.internal>
2020-10-13 21:41:37 +0000

    Se añaden hola mundos de c y ensamblador

---
+++ templete.s
@@ -0,0 +1,21 @@
+/*
+ @@ Instituto Tecnologico de Tijuana
+    @@ Depto de Sistemas y Computación
+    @@ Ing. En Sistemas Computacionales
+    @@   
+    @@ Autor : Edgar Regalado @nickname: RegaladoEdgar
+    @@ Repositorio: http://github.com/RegaladoEdgar/programas
+    @@ Fecha de revisión: ____
+    @@ 
+    @
+    @ Objetivo del programa:
+    @    
+    @
+    @ Entradas, procesos y salidas:
+    @   Thumb-2, ARM64, ...
+    @
+    @ Errores a reportar / retroalimentar
+    @   Para .....
+    @
+    @ 
+*/
